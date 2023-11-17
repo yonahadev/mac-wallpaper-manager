970405293f6cee15941fe1de265b96e8cd97d8bf
tom <83908974+goat6@users.noreply.github.com>
2023-11-17 10:31:44 +0000

    Initial Commit

---
+++ wallpaperManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wallpaperManager
+//
+//  Created by tom on 2023/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wallpaperManager/wallpaperManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallpaperManagerApp.swift
+//  wallpaperManager
+//
+//  Created by tom on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct wallpaperManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
