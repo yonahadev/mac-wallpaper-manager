f049245eac74359e42bafab13479ed5a54fcf0e7
tom <83908974+goat6@users.noreply.github.com>
2023-11-17 18:41:28 +0000

    Added selecting an individual display with a picker

--- wallpaperManager/ContentView.swift
+++ wallpaperManager/ContentView.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var status: String = "Awaiting user input..."
-    let imagePath = "/Users/tom/Documents/swift/wallpaperManager/res/john.png"
+    @State var selectedScreen: String = "None"
     var body: some View {
         VStack {
-            Text("Manage wallpapers")
-            Button("set wallpaper") {
-                manageNewWallaper()
-
+            Picker("Screen", selection:$selectedScreen) {
+                //self tells swift each element is unique allowing the for each
+                ForEach(NSScreen.screens, id: \.self) { screen in
+                    Text(screen.localizedName).tag(screen.localizedName)
+                }
             }
-            Text(status)
+            Text("Selected screen: \(selectedScreen)" )
+            Button("set wallpaper") {
+                let display = getScreenByName(selectedScreen)
+                manageNewWallpaper(display)
+            } .disabled(selectedScreen == "None")
         }
         .padding()
+        .pickerStyle(.segmented)
     }
 }
 
@@ -36,6 +41,7 @@ func chooseFile(completionHandler: @escaping (URL?) -> Void) {
     openPanel.canChooseFiles = true
     openPanel.canChooseDirectories = false
     openPanel.allowsMultipleSelection = false
+    openPanel.allowedContentTypes = [.png,.jpeg,.tiff,.gif,.bmp,.pdf]
 
     openPanel.begin { (result) in
         if result == .OK, let chosenURL = openPanel.url {
@@ -46,9 +52,8 @@ func chooseFile(completionHandler: @escaping (URL?) -> Void) {
     }
 }
 
-func setWallpaper(url:URL) -> String {
-    //guard is for early returning
-    guard let screen = NSScreen.main else {return "Failed to find screen."}
+
+func setWallpaper(url:URL, screen:NSScreen) -> String {
     do {
         try NSWorkspace.shared.setDesktopImageURL(url, for:screen)
         return "Set desktop image."
@@ -58,11 +63,33 @@ func setWallpaper(url:URL) -> String {
     }
 }
 
-let manageNewWallaper = {
+func getScreenByIndex(_ index:Int) -> NSScreen {
+    let screens = NSScreen.screens
+    //checks to see for a valid index, if not sets screen to primary (0)
+    if index < screens.count {
+        return screens[index-1]
+    } else {
+        return screens[0]
+    }
+}
+
+func getScreenByName(_ currentDisplay: String) -> NSScreen {
+    let screens = NSScreen.screens
+    var screenIndex = 0
+    for screen in screens {
+        if screen.localizedName == currentDisplay {
+            return screens[screenIndex]
+        }
+        screenIndex += 1
+    }
+    return screens[0] //returns primary if can't find name
+}
+
+func manageNewWallpaper(_ display:NSScreen) {
     chooseFile { (result) in
         //if let to continue with the rest of the code conditionally
         if let url = result {
-            let status = setWallpaper(url: url)
+            let status = setWallpaper(url: url, screen: display)
             print(status)
         } else {
             print("Invalid file url result.")
