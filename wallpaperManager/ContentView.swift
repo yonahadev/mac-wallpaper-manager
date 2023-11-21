8422bc20261c52bb4056abb8988f7ad49eb1cf54
tom <83908974+goat6@users.noreply.github.com>
2023-11-21 20:26:44 +0000

    Minor bugfixes

--- wallpaperManager/ContentView.swift
+++ wallpaperManager/ContentView.swift
@@ -11,21 +11,22 @@ import HotKey
 
 struct ContentView: View {
     @State var selectedScreen: String = getScreenByIndex(1).localizedName
-    @State var allScreens: Bool = false
+    @State var allScreens: Bool = true
     @State var imageFiles: [URL] = []
     @State var finderOpen: Bool = false
     @State var selectedIndex: Int = -1
+    @State var screens = {return NSScreen.screens}()
     
     // Setup hot key for ⌥⌘R
     let rightArrow = HotKey(key: .rightArrow, modifiers: [.command, .option])
     let leftArrow = HotKey(key: .leftArrow, modifiers: [.command, .option])
- 
+    
     var body: some View {
         VStack {
             VStack {
                 Picker("", selection:$selectedScreen) {
                     //self tells swift each element is unique allowing the for each
-                    ForEach(NSScreen.screens, id: \.self) { screen in
+                    ForEach(screens, id: \.self) { screen in
                         Text(screen.localizedName).tag(screen.localizedName)
                     }
                 }
@@ -73,7 +74,7 @@ struct ContentView: View {
                         .buttonStyle(PlainButtonStyle())
                         .background(Color.red.opacity(0.5))
                 }.padding(10)
-                }
+            }
             HStack {
                 HStack {
                     Text("Cycle backwards:")
@@ -92,43 +93,44 @@ struct ContentView: View {
                     Text("⌘⌥↑")
                         .fontWeight(.bold)
                         .background(Color.accentColor.opacity(0.5))
-                        
+                    
                 }
             } .padding(5)
             ScrollView {
-                let columns = [GridItem(.adaptive(minimum: 100,maximum: 200))]
-                LazyVGrid(columns: columns) {
-                    ForEach(Array(zip(imageFiles.indices,imageFiles)), id: \.0) { index, imageURL in
-                        VStack{
-                            Button {
-                                selectedIndex = index
-                                handleWallpaperChange(allScreens: allScreens, image: imageURL, target: selectedScreen)
-                            } label: {
-                                let image = getNSImageFromURL(imageURL)
-                                Image(nsImage:image)
-                                    .resizable()
-                                    .frame(width:160,height:90)
-                                    .aspectRatio(contentMode: .fill)
-                                    .shadow(radius: 10)
-                                    .border(index == selectedIndex ? Color.accentColor : Color.clear, width: 1)
-                            } .buttonStyle(PlainButtonStyle())
-                            Image(systemName: "trash")
-                                .foregroundColor(.white)
-                                .padding(7)
-                                .shadow(radius:5)
-                                .background(Color.red)
-                                .clipShape(Circle())
-                                .onTapGesture {
-                                    imageFiles.remove(at: index)
-                                    removeURLFromFile(imageURL)
-                                }
-                        }
-                        }
-                    } .padding()
-                } .frame(minWidth: 400, minHeight:200,maxHeight: 400)
-                    .background(Color.white.opacity(0.1))
+               let columns = [GridItem(.adaptive(minimum: 100,maximum: 200))]
+               LazyVGrid(columns: columns, spacing: 20) { // Add spacing here
+                   ForEach(Array(zip(imageFiles.indices,imageFiles)), id: \.0) { index, imageURL in
+                       VStack{
+                           Button {
+                              selectedIndex = index
+                              handleWallpaperChange(allScreens: allScreens, image: imageURL, target: selectedScreen)
+                           } label: {
+                              let image = getNSImageFromURL(imageURL)
+                              Image(nsImage:image)
+                                  .resizable()
+                                  .frame(width:100,height:100)
+                                  .aspectRatio(contentMode: .fill)
+                                  .shadow(radius: 10)
+                                  .border(index == selectedIndex ? Color.accentColor : Color.clear, width: 1)
+                           } .buttonStyle(PlainButtonStyle())
+                           Image(systemName: "trash")
+                              .foregroundColor(.white)
+                              .padding(7)
+                              .shadow(radius:5)
+                              .background(Color.red)
+                              .clipShape(Circle())
+                              .onTapGesture {
+                                  imageFiles.remove(at: index)
+                                  removeURLFromFile(imageURL)
+                              }
+                           }
+                       }
+                       .padding() // Add padding here
+                   }.padding() // Add padding here
+               }
+               .frame(minWidth: 400, minHeight:200,maxHeight: 400)
+               .background(Color.white.opacity(0.1))
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .padding()
             .onAppear() {
                 rightArrow.keyDownHandler = {
@@ -146,8 +148,12 @@ struct ContentView: View {
                     }
                 }
             }
+            .onReceive(NotificationCenter.default.publisher(for: NSApplication.didChangeScreenParametersNotification)) { _ in
+                screens = NSScreen.screens
+            }
+            
         }
-}
+    }
 
 #Preview {
     ContentView()
